f9a450da914c0001d145c786e0bce3ec7330e5c3
Matvey Bashlykov <bashmatred@gmail.com>
2025-02-03 23:54:04 +0300

    Add tray icon with menu

---
+++ MathModelSimulator/src/MathModelSimulator.Application/ViewModels/AppViewModel.cs
@@ -0,0 +1,27 @@
+﻿#region Usings
+
+using System.Windows.Input;
+using Avalonia.Controls.ApplicationLifetimes;
+using CommunityToolkit.Mvvm.Input;
+
+#endregion
+
+namespace MathModelSimulator.Application.ViewModels;
+
+public class AppViewModel : ViewModelBase
+{
+    public ICommand CloseCommand { get; init; }
+
+    public AppViewModel()
+    {
+        CloseCommand = new RelayCommand(OnCloseCommandExecuted);
+    }
+
+    private void OnCloseCommandExecuted()
+    {
+        if (Avalonia.Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.Shutdown();
+        }
+    }
+}
