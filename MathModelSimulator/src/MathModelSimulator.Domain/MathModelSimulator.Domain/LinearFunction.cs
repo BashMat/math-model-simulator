fd2bfedf39f2d3749cb291479ea195c3c19f2316
Matvey Bashlykov <bashmatred@gmail.com>
2025-02-05 01:28:18 +0300

    Add simple linear plot

---
+++ MathModelSimulator/src/MathModelSimulator.Domain/MathModelSimulator.Domain/LinearFunction.cs
@@ -0,0 +1,33 @@
+﻿namespace MathModelSimulator.Domain;
+
+public class LinearFunction
+{
+    public LinearFunctionParameters Parameters { get; set; }
+    
+    public LinearFunction()
+    {
+        Parameters = new LinearFunctionParameters();
+    }
+    
+    public LinearFunction(LinearFunctionParameters parameters)
+    {
+        Parameters = parameters;
+    }
+
+    public double CalculateSingleValue(double input)
+    {
+        return Parameters.Slope * input + Parameters.InitialValue;
+    }
+    
+    public IEnumerable<Point2D> CalculatePoints(IReadOnlyCollection<double> input)
+    {
+        foreach (var value in input)
+        {
+            yield return new Point2D
+                         {
+                             X = value,
+                             Y = CalculateSingleValue(value)
+                         };
+        }
+    }
+}
---
+++ MathModelSimulator/src/MathModelSimulator.Domain/MathModelSimulator.Domain/LinearFunctionParameters.cs
@@ -0,0 +1,7 @@
+﻿namespace MathModelSimulator.Domain;
+
+public class LinearFunctionParameters
+{
+    public double Slope { get; set; }
+    public double InitialValue { get; set; }
+}
